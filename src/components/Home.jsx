91882f2263056f2c6e8e7c1f7bd1bcbe86501e99
himudit <gargmudit662@gmail.com>
2024-09-02 14:29:01 +0530

    adding expenses

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,30 +4,44 @@ function Home() {
   const [budget, setBudget] = useState("Enter Budget First");
   const [expenses, setExpenses] = useState(0);
 
+  // budget
   const enterBudget = (e) => {
     setBudget(e.target.value);
   };
-
   const handleFocus = () => {
     if (budget === "Enter Budget First") {
       setBudget("");
     }
   };
 
+  // expenses
+
+
   return (
     <div className="flex flex-col items-center justify-between min-h-screen bg-gray-100">
+      {/* expenses */}
       <div className="flex w-full space-x-4 p-4">
         <div className="bg-white p-6 rounded-lg shadow-md flex-1">
-          <input 
-            type="text" 
-            value={budget} 
-            onChange={enterBudget} 
+          Total revenue
+          <span className="mr-2">$</span>
+          <input
+            type="text"
+            value={budget}
+            onChange={enterBudget}
             onFocus={handleFocus}
+            placeholder="Enter Budget"
             className="w-full p-2 border border-gray-300 rounded-lg"
           />
         </div>
         <div className="bg-white p-6 rounded-lg shadow-md flex-1">
-          {expenses}
+          Total expense
+          <span className="mr-2">$</span>
+          <input
+            type="text"
+            value={expenses}
+            placeholder=""
+            className="w-full p-2 border border-gray-300 rounded-lg"
+          />
         </div>
       </div>
       <div className="bg-white p-10 rounded-lg shadow-md flex-grow w-full m-4">
